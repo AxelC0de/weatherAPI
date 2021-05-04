4014a14f169fdbfd06850f553aae8c0e55d8122e
AxelC0de <AxelC0de.github@gmail.com>
2021-05-04 13:27:39 +0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # weatherAPI
+Практика по массивам - получаем прогноз погоды по API
 https://www.youtube.com/watch?v=2tiVdao_E1s&list=WL&index=1&t=29s
+
+API: b0b4ebc287e2817f902eddbfdaded9d1
+axelc0de
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=b0b4ebc2
 		document.querySelector('.cityname').textContent = data.name;
 		document.querySelector('.temp').innerHTML = Math.round((data.main.temp - 273)) + '&deg;';
 		document.querySelector('.description').textContent = data.weather[0]['description']
-		document.querySelector('.icon').innerHTML = `<img src = `"http://openweathermap.org/img/wn/${data.main[0]['icon']}@2x.png">`;
+		// document.querySelector('.icon').innerHTML = `<img src = `"http://openweathermap.org/img/wn/${data.main[0]['icon']}@2x.png">`;
 		// document.querySelector('.icon').innerHTML = <img src= 'http://openweathermap.org/img/wn/10d@2x.png'></img>
 			// http://openweathermap.org/img/wn/10d@2x.png
 			})
@@ -13,50 +13,3 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=b0b4ebc2
 				// catch errors
 			})
 
-
-// {
-// 	"coord": {
-// 		"lon": 37.6156,
-// 			"lat": 55.7522
-// 	},
-// 	"weather": [
-// 		{
-// 			"id": 501,
-// 			"main": "Rain",
-// 			"description": "moderate rain",
-// 			"icon": "10d"
-// 		}
-// 	],
-// 		"base": "stations",
-// 			"main": {
-// 		"temp": 282.31,
-// 			"feels_like": 279.33,
-// 				"temp_min": 282.15,
-// 					"temp_max": 282.59,
-// 						"pressure": 1001,
-// 							"humidity": 93
-// 	},
-// 	"visibility": 8000,
-// 		"wind": {
-// 		"speed": 6,
-// 			"deg": 120
-// 	},
-// 	"rain": {
-// 		"1h": 3.28
-// 	},
-// 	"clouds": {
-// 		"all": 90
-// 	},
-// 	"dt": 1620022047,
-// 		"sys": {
-// 		"type": 1,
-// 			"id": 9027,
-// 				"country": "RU",
-// 					"sunrise": 1620006120,
-// 						"sunset": 1620061842
-// 	},
-// 	"timezone": 10800,
-// 		"id": 524901,
-// 			"name": "Moscow",
-// 				"cod": 200
-// }
